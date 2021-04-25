6e75f810c39fed852021b031b0b11e6276ff95d1
Mikhail Martusevich <61042574+psxzz@users.noreply.github.com>
2021-04-25 07:58:44 -0500

    Add files via upload

--- Data Bases/Lab01/1_Tables_Create.sql
+++ Data Bases/Lab01/1_Tables_Create.sql
@@ -1,22 +1,22 @@
 USE Manufacture
 
 CREATE TABLE Изделия (
-	[Наименование изделия] NVARCHAR(20) NOT NULL PRIMARY KEY,
+	[Наименование изделия] NVARCHAR(20) NOT NULL,
 	[Стоимость] INTEGER DEFAULT 0
 )
 
 CREATE TABLE Комплектация (
-	[Наименование изделия] NVARCHAR(20) NOT NULL FOREIGN KEY REFERENCES Изделия([Наименование изделия]),
-	[Наименование комплектующего] NVARCHAR(20) NOT NULL PRIMARY KEY,
+	[Наименование изделия] NVARCHAR(20) NOT NULL,
+	[Наименование комплектующего] NVARCHAR(20)NOT NULL,
 	[Количество] INTEGER DEFAULT 0
 )
 
 CREATE TABLE Комплектующее (
-	[Наименование комплектующего] NVARCHAR(20) NOT NULL FOREIGN KEY REFERENCES Комплектация([Наименование комплектующего]),
+	[Наименование комплектующего] NVARCHAR(20)NOT NULL,
 	[Стоимость] INTEGER DEFAULT 0
 )
 
 CREATE TABLE Заказы (
-	[Наименование изделия] NVARCHAR(20) NOT NULL FOREIGN KEY REFERENCES Изделия([Наименование изделия]),
+	[Наименование изделия] NVARCHAR(20) NOT NULL,
 	[Количество] INTEGER DEFAULT 0
 )
